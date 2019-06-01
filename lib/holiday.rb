34eb9b6a26da57e706ed66500ccc6e8562beaab2
kevin lang <langkevina@gmail.com>
2019-06-01 18:32:52 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -80,6 +80,7 @@ def all_supplies_in_holidays(holiday_hash)
 	      # binding.pry
         str = attribute.to_s
         str.sub!("_", " ")
+        str.split.map { |x| x.capitalize }.join(" ")
         puts "  #{str.capitalize}: " + "#{data.join(", ")}"
       end
 
